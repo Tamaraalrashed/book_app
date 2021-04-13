cde6570cd08223b956f0046787ef3923d79b6f29
Tamara <tamara_almanaseer@live.com>
2021-04-13 14:46:47 +0300

    enhanced one

--- server.js
+++ server.js
@@ -4,10 +4,12 @@
 require('dotenv').config();
 
 const { query } = require('express');
-// const { query } = require('express');
+
 const express=require('express');
 const superagent = require('superagent');
-
+// const path = require( 'path' );
+// server.set( 'views', path.join( __dirname, '/views/pages' ) );
+// server.set( 'view engine','ejs' );
 const PORT = process.env.PORT || 3030;
 const server=express();
 server.set('view engine','ejs');
@@ -24,21 +26,21 @@ server.get('/',(req,res)=>{
      res.render('pages/index');
 })
 
-//localhost:3000/search/new
-server.get('/search/new',(req,res)=>{
+//localhost:3000/searches/new
+server.get('/searches/new',(req,res)=>{
   
          res.render('pages/searches/new');
     })
 
 //localhost:3000/search
-server.post('/search',(req,res)=>{
+server.post('/searches',(req,res)=>{
       let searchedBook=req.body.q;
       let searchBy=req.body.searchBy;
       console.log(req.body);
       let URL=`https://www.googleapis.com/books/v1/volumes?q=+in${searchBy}:${searchedBook}&maxResults=10`
          superagent.get(URL)
          .then(booksData=>{
-             console.log('by title', booksData.body.items);
+            //  console.log('by title', booksData.body.items);
              let bookDataArr= booksData.body.items.map((item)=>{
                 let newBook= new Books (item);
                 return newBook;
@@ -47,35 +49,15 @@ server.post('/search',(req,res)=>{
             res.render('pages/searches/shows',{books:bookDataArr} );
          })
 
-    //       if (req.body.title=='on'){
-    //           let URL=`https://www.googleapis.com/books/v1/volumes?q=${searchedBook}+intitle:keys&maxResults=10`
-    //      superagent.get(URL)
-    //      .then(booksData=>{
-    //          console.log('by title', booksData.body.items);
-    //          let titleDataArr= booksData.body.items.map((item)=>{
-    //             let titleBook= new Books (item);
-    //             return titleBook;
-    //               });
-    
-    //         res.render('pages/searches/shows',{books:titleDataArr} );
-    //     }) }
-    //     if (req.body.author=='on'){
-    //         let URL=`https://www.googleapis.com/books/v1/volumes?q=${searchedBook}+inauthor:keys&maxResults=10`
-    //    superagent.get(URL)
-    //    .then(booksData=>{
-    //        console.log('by author', booksData.body);
-    //        res.render('pages/searches/shows',{books:titleDataArr} );
-    //   })   
-    //       }
        
     })
 
 
 function Books(book){
- this.image=book.volumeInfo.imageLinks.thumbnail;
+ this.image=(book.volumeInfo.imageLinks)?book.volumeInfo.imageLinks.thumbnail:"https://i.imgur.com/J5LVHEL.jpg";
 this.title=book.volumeInfo.title;
-this.author=book.volumeInfo.authors;
-this.description=book.volumeInfo.description;
+this.author=(book.volumeInfo.authors)? book.volumeInfo.authors: 'No author data for this book';
+this.description=(book.volumeInfo.description)?book.volumeInfo.description:'No description data for this book';
 }
 
 //localhost:3000/*
